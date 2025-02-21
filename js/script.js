997d4445181bfd7871dd7aab24bcb16e5fe8a8f1
Monitoj10 <fanasforever@gmail.com>
2025-02-20 19:22:21 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,1 @@
-</script>
-<elevenlabs-convai agent-id="FVTxUiLXvq4vQAvbtR8o"></elevenlabS-convai><script src="https://elevenlabs.io/convai-widget/index.js" async type="text/javascript"> </script/
+<elevenlabs-convai agent-id="FVTxUiLXvq4vQAvbtR8o"></elevenlabs-convai><script src="https://elevenlabs.io/convai-widget/index.js" async type="text/javascript"></script>
